5d021e8add8b0d91fa2c556234c15c16c91ca6ab
student <student@ELC-MAC-AIR-05.local>
2019-08-08 10:11:40 -0500

    changes to Internship and main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 #main.py
 # the import section
 import webapp2
-import jinja2
 import os
+import jinja2
 from google.appengine.ext import ndb
+
 
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
@@ -22,7 +23,12 @@ class InternshipsPage(webapp2.RequestHandler):
     def get(self): #for a get request
         self.response.headers['Content-Type'] = 'text/html'
         welcometemplate = JINJA_ENVIRONMENT.get_template('Template/FCD_Internships.html')
-        self.response.write(welcometemplate.render())
+        ships = get_all_internships()
+        template_values = {
+            'ships': ships, 
+            'last updated': 'today',
+        }
+        self.response.write(welcometemplate.render(template_values))
     
 
         
@@ -33,15 +39,30 @@ class ContactPage(webapp2.RequestHandler):
         self.response.write(welcometemplate.render())
 
 
+
 class AboutPage(webapp2.RequestHandler):
     def get(self): #for a get request
         self.response.headers['Content-Type'] = 'text/html'
         welcometemplate = JINJA_ENVIRONMENT.get_template('Template/FCD_About_page.html')
         self.response.write(welcometemplate.render())
 
+
+class Internships(ndb.Model):
+    #list here all the properties of the entity
+    Title = ndb.StringProperty(required=True)
+    Description = ndb.StringProperty(required=True)
+    Link = ndb.StringProperty(required=True)
+    
+def get_all_internships():
+    intern = Internships.query().fetch()
+    print "intern"
+    return intern 
+
         
 # the app configuration section
 app = webapp2.WSGIApplication([
-    ('/', HomePage),  ('/internships', InternshipsPage), ('/contact', ContactPage),
+    ('/', HomePage), 
+     ('/internships', InternshipsPage), 
+     ('/contact', ContactPage),
     ('/aboutpage', AboutPage),#this maps the root url to the Main Page Handler
 ], debug=True)
